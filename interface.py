59f82234fc260c5e79d0e67f03cbed27e3fa9be6
Thiago Wriel <140001164+ThiagoWriel@users.noreply.github.com>
2024-05-21 15:02:25 -0300

    Criando a base da interface

--- interface.py
+++ interface.py
@@ -1,4 +1,10 @@
 import customtkinter as ctk
+from PIL import Image
+import os
+
+def get_absolute_path(relative_path):
+    base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 #definição do menu principal
 class MainMenu(ctk.CTk):
@@ -11,11 +17,24 @@ class MainMenu(ctk.CTk):
         
         #definição de etiquetas
         self.main_menu_label = ctk.CTkLabel(self, text="Menu Principal", font=("Poppins", 20))
-        self.main_menu_label.pack(pady=25)
+        self.main_menu_label.pack(pady=50)
         
-        #botão do menu de climatizadores
-        self.button1 = ctk.CTkButton(self, text="Climatizadores", command=self.abrir_menu1)
-        self.button1.pack(pady=25)
+        #botão do menu dos Ar Condicionado
+        img = ctk.CTkImage(light_image=Image.open("/volume.png"), 
+                           dark_image=Image.open("/volume.png"),
+                           size = (20,20))
+        self.button1 = ctk.CTkButton(self, 
+                                     text="Ar Condicionado", 
+                                     command=self.abrir_menu1, 
+                                     width = 300,
+                                     height=50,
+                                     fg_color="blue",
+                                     hover_color= "#4e31ff",
+                                     text_color="white",
+                                     font = ("arial bold", 18),
+                                     corner_radius=20,
+                                     state = "normal",
+                                     image=img).pack(pady=25)
         
         #botão do menu de lâmpadas
         self.button2 = ctk.CTkButton(self, text="Lâmpadas", command=self.abrir_menu2)
@@ -25,8 +44,12 @@ class MainMenu(ctk.CTk):
         self.button3 = ctk.CTkButton(self, text="Televisões", command=self.abrir_menu3)
         self.button3.pack(pady=25)
         
+        #botão adicionar dispositivo
+        self.button4 = ctk.CTkButton (self, text = "Novo dispositivo", command = self.abrir_menu4)
+        self.button4.pack (pady=25)
+
     def abrir_menu1(self) -> None:
-        MenuClimatizador()
+        MenuAr()
     
     def abrir_menu2(self) -> None:
         MenuLampada()
@@ -34,17 +57,20 @@ class MainMenu(ctk.CTk):
     def abrir_menu3(self) -> None:
         MenuTelevisao()
 
+    def abrir_menu4 (self) -> None:
+        MenuAdicionarDispositivo()
 
-class MenuClimatizador(ctk.CTkToplevel):
+
+class MenuAr(ctk.CTkToplevel):
     def __init__(self):
         super().__init__()
         
         #definição da janela
-        self.title("Menu de Climatizadores")
+        self.title("Menu do Ar")
         self.geometry("400x240")
         
         #definição de etiquetas
-        self.label = ctk.CTkLabel(self, text="Climatizadores", font=("Poppins", 20))
+        self.label = ctk.CTkLabel(self, text="Ar condicionados", font=("Poppins", 20))
         self.label.pack(pady=10)
 
         #criação de um slider
@@ -71,7 +97,7 @@ class MenuLampada(ctk.CTkToplevel):
         super().__init__()
         
         #definição da janela
-        self.title("Menu de Lâmpadas")
+        self.title("Menu das Lâmpadas")
         self.geometry("400x240")
         
         #definição de etiquetas
@@ -87,7 +113,7 @@ class MenuTelevisao(ctk.CTkToplevel):
         super().__init__()
         
         #definição da janela
-        self.title("Menu de Televisões")
+        self.title("Menu dos Televisões")
         self.geometry("400x240")
         
         #definição de etiquetas
@@ -98,6 +124,39 @@ class MenuTelevisao(ctk.CTkToplevel):
         self.back_button = ctk.CTkButton(self, text="Voltar", command=self.destroy)
         self.back_button.pack(pady=10)
 
+class MenuAdicionarDispositivo (ctk.CTkToplevel):
+    def __init__(self):
+        super().__init__()
+        
+        #definição da janela
+        self.title("Novo Dispositivo")
+        self.geometry("400x240")
+        
+        #definição de etiquetas
+        self.label = ctk.CTkLabel(self, text="Novo Dispositivo", font=("Poppins", 20))
+        self.label.pack(pady=10)
+
+        # Menu de opções
+        self.opcoes = ["Lâmpada", "Ar Condicionado", "Televisão"]
+        self.option_menu = ctk.CTkOptionMenu(self, values=self.opcoes)
+        self.option_menu.pack(pady=10)
+
+        # Botão de confirmação
+        self.confirm_button = ctk.CTkButton(self, text="Confirmar", command=self.confirmar_dispositivo)
+        self.confirm_button.pack(pady=10)
+        
+        # Botão retornar
+        self.back_button = ctk.CTkButton(self, text="Voltar", command=self.destroy)
+        self.back_button.pack(pady=10)
+        
+        #botão retornar
+        self.back_button = ctk.CTkButton(self, text="Voltar", command=self.destroy)
+        self.back_button.pack(pady=10)
+
+    def confirmar_dispositivo(self):
+        escolha = self.option_menu.get()
+        print(f"Dispositivo selecionado: {escolha}")
+        self.destroy()
 
 if __name__ == "__main__":
     app = MainMenu()
