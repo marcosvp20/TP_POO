736b0b6a16cd519de6d8276e43d17f16707dcf29
Marcos Azevedo <marcosviniciuspinheiro63@gmail.com>
2024-05-23 13:44:03 -0300

    modularizacao da interface

---
+++ frame_inferior.py
@@ -0,0 +1,27 @@
+import customtkinter as ctk
+
+class FrameInferior:
+    
+    def __init__(self, janela) -> None:
+        self.janela = janela
+    
+    def frameinferior(self) -> None:
+        self.frame_inferior = ctk.CTkFrame(master=self.janela, width=800, height=100, fg_color="#2a2a2a", corner_radius=0)
+        self.frame_inferior.place(x = 0, y = 500)
+
+    def botaodispositivos(self) -> None:
+        self.botao_dispositivos = ctk.CTkButton(master=self.frame_inferior, width=300, height=100, 
+                                                corner_radius=35, text='Dispositivos',font= ('Rockwell', 15), 
+                                                fg_color='transparent', hover_color='#4e31ff')
+        self.botao_dispositivos.place(y = 0, x = 50)
+    
+    def botaoautomacao(self) -> None:
+        self.botao_automacao = ctk.CTkButton(master=self.frame_inferior, width=300, height=100, 
+                                             corner_radius=35, text='Automação', font= ('Rockwell', 15),
+                                             fg_color='transparent', hover_color='#4e31ff')
+        self.botao_automacao.place(y = 0, x = 450)
+    
+    def executar(self) -> None:
+        self.frameinferior()
+        self.botaoautomacao()
+        self.botaodispositivos()
