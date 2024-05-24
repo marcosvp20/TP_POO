facf1adc841e48e8e1d3e3071000da3e57923dfe
Marcos Azevedo <marcosviniciuspinheiro63@gmail.com>
2024-05-24 13:37:54 -0300

    interface de automações

---
+++ interfaceAutomações.py
@@ -0,0 +1,67 @@
+import customtkinter as ctk
+
+class interfaceAutomacoes:
+    def __init__(self, janela) -> None:
+        self.janela = janela
+        
+        
+    def criaframe(self) -> None:
+         self.frame_automacoes = ctk.CTkFrame(self.janela, width=800, height=500, fg_color='black', corner_radius=0)
+        
+    def textosuperior(self) -> None:
+        self.texto_superior = ctk.CTkLabel(self.frame_automacoes, width=200, height=50, text='Automações',
+                                           font=('Rockweel', 25), text_color='#cbcbcb')
+        self.texto_superior.place(x = 0, y = 15)
+    
+    def botao1(self) -> None:
+        self.botao_1 = ctk.CTkButton(master=self.frame_automacoes, text='Bom dia',
+                                    font=('Rockwell', 15), width=300, height=55, fg_color='#2a2a2a',
+                                    corner_radius=35)
+        self.botao_1.place(x = 25, y = 125)
+    
+    def botao2(self) -> None:
+        self.botao_2 = ctk.CTkButton(master=self.frame_automacoes, text='Chengando em\ncasa',
+                                    font=('Rockwell', 15), width=300, height=55, fg_color='#2a2a2a',
+                                    corner_radius=35)
+        self.botao_2.place(x = 25, y = 215)
+    
+    def botao3(self) -> None:
+        self.botao_3 = ctk.CTkButton(master=self.frame_automacoes, text='Hora da\nlimpeza',
+                                    font=('Rockwell', 15), width=300, height=55, fg_color='#2a2a2a',
+                                    corner_radius=35)
+        self.botao_3.place(x = 25, y = 305)
+    
+    def botao4(self) -> None:
+        self.botao_4 = ctk.CTkButton(master=self.frame_automacoes, text='Hora da\nlimpeza',
+                                    font=('Rockwell', 15), width=300, height=55, fg_color='#2a2a2a',
+                                    corner_radius=35)
+        self.botao_4.place(x = 475, y = 125)
+    
+    def botao5(self) -> None:
+        self.botao_5 = ctk.CTkButton(master=self.frame_automacoes, text='Hora de\nestudar',
+                                    font=('Rockwell', 15), width=300, height=55, fg_color='#2a2a2a',
+                                    corner_radius=35)
+        self.botao_5.place(x = 475, y = 215)
+        
+    def botao6(self) -> None:
+        self.botao_6 = ctk.CTkButton(master=self.frame_automacoes, text='Hora de\ndormir',
+                                    font=('Rockwell', 15), width=300, height=55, fg_color='#2a2a2a',
+                                    corner_radius=35)
+        self.botao_6.place(x = 475, y = 305)
+    
+    def botaoadicionar(self) -> None:
+        self.botao_adicionar = ctk.CTkButton(master=self.frame_automacoes, text='Adicionar',
+                                    font=('Rockwell', 15), width=150, height=100, fg_color='#2a2a2a',
+                                    corner_radius=35)
+        self.botao_adicionar.place(x = 600, y = 395)
+    def executar(self) -> None:
+        self.criaframe()
+        self.textosuperior()
+        self.botao1()
+        self.botao2()
+        self.botao3()
+        self.botao4()
+        self.botao5()
+        self.botao6()
+        #self.botaoadicionar()
+        self.frame_automacoes.place(x = 0, y = 0)
