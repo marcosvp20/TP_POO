808d7517cc42ea2fbd1f14d52d9a9773eee12d0b
Marcos Azevedo <marcosviniciuspinheiro63@gmail.com>
2024-05-24 14:00:53 -0300

    mudando o tema para claro

--- janela_principal.py
+++ janela_principal.py
@@ -4,7 +4,8 @@ from frame_inferior import FrameInferior
 class JanelaPrincipal:
     
     def __init__(self) -> None:
-        self.janela_principal = ctk.CTk(fg_color='Black')
+        ctk.set_appearance_mode('light')
+        self.janela_principal = ctk.CTk(fg_color='white')
         self.janela_principal.title('Main')
         self.janela_principal.geometry('800x600')
         frame_1 = FrameInferior(self.janela_principal)
