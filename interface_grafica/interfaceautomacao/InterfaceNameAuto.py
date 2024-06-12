79916a9d79358907c795631c1e313e7a59fd9f80
Henrique Carneiro <henriquecctn@gmail.com>
2024-06-11 21:32:45 -0300

    Tentativa de salvar apenas alterações às automações

--- interface_grafica/interfaceautomacao/InterfaceNameAuto.py
+++ interface_grafica/interfaceautomacao/InterfaceNameAuto.py
@@ -54,8 +54,9 @@ class InterfaceNameAuto:
         """
         self.__nome_auto = self.txtbox_name_auto.get().strip()
         auto = Automacao(self.__nome_auto)
+        auto.planilha_auto_temp.comparar_e_apagar_linhas('planilhas/objetos.xlsx')
         if auto.adicionar_auto():
-            self.__mensagem_confirmacao('     Automação adicionada com sucesso!!')
+            self.__mensagem_confirmacao('Automação adicionada com sucesso!!')
         else:
             self.__mensagem_confirmacao('Já existe uma automação com esse apelido!')
         
